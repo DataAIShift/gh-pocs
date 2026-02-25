21011b7fc836edb3f69dfc8c6661da4f099e8712
pratik-sapkota007 <pratik.sapkota@shell.com>
2026-02-25 11:35:09 +0530

    fix all

--- prerequisities/providers.tf
+++ prerequisities/providers.tf
@@ -11,9 +11,6 @@ terraform {
  
 provider "azurerm" {
   features {}
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-  client_id       = var.client_id
   use_oidc        = true
   use_cli         = false
 }
